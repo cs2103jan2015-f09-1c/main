3551cb1f872ee4e4b5f065c55d5d5480384f739c
seowyanyi <seow.yanyi@gmail.com>
2015-03-19 22:48:26 +0800

    easy logging integration

--- MyCal/main.cpp
+++ MyCal/main.cpp
@@ -3,8 +3,22 @@
 #include "TextUI.h"
 #include "Storage.h"
 #include <iostream>
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+void configureLogging() {
+    el::Configurations defaultConf;
+    defaultConf.setToDefault();
+    defaultConf.setGlobally(el::ConfigurationType::ToStandardOutput, "false");
+    el::Loggers::reconfigureLogger("default", defaultConf);
+    LOG(INFO) << "LOG SESSION BEGIN <<<<<<<<<<<";
+}
 
 int main(int argc, char *argv[]) {
+    START_EASYLOGGINGPP(argc, argv);
+    configureLogging();
+
 	Controller controllerService;
 
 	TextUI::printWelcomeMsg();
@@ -18,7 +32,7 @@ int main(int argc, char *argv[]) {
     tasksToday.setTaskList(today);
     TextUI::showOutput(tasksToday);
 	
-	while (true) {
+	while (true) {        
 		std::string userInput;
 		userInput = TextUI::getInput();
 
