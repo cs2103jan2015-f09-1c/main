43c1e3f6817bda890e588ea78dbb4d03886b729c
ratnakwanditanto <ratnakwanditanto@gmail.com>
2015-03-24 11:21:02 +0800

    MappingNumber (TaskID & displayed number)

---
+++ History/MappingNumber.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <list>
+
+class MappingNumber{
+public:
+	MappingNumber(void);
+	~MappingNumber(void);
+
+	struct NODE {
+		unsigned taskID;
+		unsigned displayNum;
+	};
+	typedef std::list<NODE> node;
+	typedef std::list<NODE>::iterator node_iterator;
+    typedef std::list<NODE>::const_iterator const_node_iterator;
+
+	static MappingNumber* getInstance();
+    static void resetInstance(); 
+	void addNode(NODE node);
+	void clearMappingNumber(); // everytime a fresh set of tasks is displayed on textUI, and we want to add a fresh set of nodes, we should clear everything in the vector. If not will have duplicates
+	unsigned getTaskID(unsigned num) const;// pass in a number on screen,
+	//should return corresponding taskID
+
+private:
+	node _displayMapping;
+	static MappingNumber* _instance;
+	
+};
