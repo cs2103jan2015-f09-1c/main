7aa1b00b613cfb27fd0fcdd1a245313b9b0e9153
ratnakwanditanto <ratnakwanditanto@gmail.com>
2015-03-24 18:09:19 +0800

    view command -> currently still for getAll, getToday and getTomorrow

--- Command/ViewCmd.cpp
+++ Command/ViewCmd.cpp
@@ -11,32 +11,39 @@ ViewCmd::~ViewCmd(void) {
 
 void ViewCmd:: prepareDetail(std::string detail){
     _detail = detail;
+}
 
+void ViewCmd:: getSelectedTasks(){
+	
+	//get current tasks
+    Storage* storage = Storage::getInstance();
+    TaskList taskList = storage->getTaskList();
+	
+	//returns the day's tasks
+	 if(_detail == "all"){
+		selectedTasks = taskList.getAll();
+	} else
+	if(_detail == "today"){
+		selectedTasks = taskList.getToday();
+	}else
+	if(_detail == "tomorrow"){
+		selectedTasks = taskList.getTomorrow();
+	}
+	// havent made function for other types
 }
 
 
 UIObject ViewCmd::execute() {
-    //get current tasks
-    Storage* storage = Storage::getInstance();
-    TaskList taskList = storage->getTaskList();
 	std:: string header;
 	
-
-    //returns the day's tasks
-    TaskList::TList wantedTasks;
-    if(_detail == "all"){
-		wantedTasks = taskList.getAll();
-	}
-
+	getSelectedTasks();
+   
     UIObject viewObj;
-	if(!wantedTasks.empty()){
-	viewObj.setTaskList(wantedTasks);
-	} 
-	else{
+	if(!selectedTasks.empty()){
+		viewObj.setTaskList(selectedTasks);
+	} else{
 		viewObj.setHeaderText("There is no task!");
 	}
-
-
 	return viewObj;
 }
 
