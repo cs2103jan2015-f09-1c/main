d87595038932f18e9ab46d257c9cea1846d84ead
seowyanyi <seow.yanyi@gmail.com>
2015-03-29 22:41:14 +0800

    add TaskStub for tasks

--- MyCalTestSuite/StorageTest.cpp
+++ MyCalTestSuite/StorageTest.cpp
@@ -11,68 +11,6 @@ namespace MyCalTestSuite {
     
 	TEST_CLASS(StorageTest) {
 	public:
-        time_t sampleTime() {
-            time_t rawTime;
-            tm sampleTime;
-            sampleTime.tm_hour = 7;
-            sampleTime.tm_isdst = 0;
-            sampleTime.tm_mday = 9;
-            sampleTime.tm_min = 23;
-            sampleTime.tm_mon = 2;
-            sampleTime.tm_sec = 45;
-            sampleTime.tm_wday = 1;
-            sampleTime.tm_yday = 67;
-            sampleTime.tm_year = 115;
-           
-            rawTime = mktime(&sampleTime);
-            return rawTime;
-        }
-
-        Task sampleTask1() {
-            Task task;
-            task.setTaskID((unsigned) 1);
-            task.setTaskName("cs2103 tutorial");
-            task.setTaskBegin(sampleTime());
-            task.setTaskEnd(sampleTime() + 5400);
-            return task;
-        }
-
-        Task sampleTask2() {
-            Task task;
-            task.setTaskID((unsigned) 3);
-            task.setTaskName("Performance @ UCC");
-            task.setTaskBegin(sampleTime() - 180000);
-            task.setTaskEnd(sampleTime() - 172800);
-            task.markDone();
-            return task;
-        }
-
-        Task sampleTask3() {
-            Task task;
-            task.setTaskID((unsigned) 4);
-            task.setTaskName("Buy gift for John");
-            task.markDone();
-            return task;
-        }
-
-        Task sampleTask4() {
-            Task task;
-            task.setTaskID((unsigned) 5);
-            task.setTaskName("Go to NTU");
-            task.setTaskBegin(sampleTime() - 72000);
-            task.setTaskEnd(sampleTime() - 63000);
-            return task;
-        }
-
-        TaskList sampleTaskList() {
-            TaskList list;
-            list.add(sampleTask3());
-            list.add(sampleTask1());
-            list.add(sampleTask2());
-            list.add(sampleTask4());
-            return list;
-        }
-
         bool isFileExist(const char *fileName) {
             std::ifstream infile(fileName);
             return infile.good();
@@ -136,7 +74,7 @@ namespace MyCalTestSuite {
             createSettingsFile(initialTaskListLoc);
 
             Storage *storage = Storage::getInstance();
-            storage->updateStorage(sampleTaskList());
+            storage->updateStorage(TaskStub::getSampleTaskList());
 
             //File not moved yet and should exist
             Assert::IsTrue(isFileExist(initialFile.c_str()));
@@ -170,7 +108,7 @@ namespace MyCalTestSuite {
             createSettingsFile(taskListLoc);
 
             Storage *storage = Storage::getInstance();
-            storage->updateStorage(sampleTaskList());
+            storage->updateStorage(TaskStub::getSampleTaskList());
             TaskList listAfterUpdate = storage->getTaskList();
             std::ostringstream oss;
             oss << "Buy gift for John" << std::endl;
--- TestUtilities/TaskStub.h
+++ TestUtilities/TaskStub.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "TaskList.h"
+
 class TaskStub {
+private:
+    // given a type time_t, returns local time in the format
+    // DD/MM/YY HH:MM AM
+    static std::string TaskStub::timetToString(time_t timing);
+    // returns an arbitrary hard coded time 09/03/15 07:23 AM
+    static time_t getSampleTime();
+    static Task getSampleTask1();
+    static Task getSampleTask2();
+    static Task getSampleTask3();
+    static Task getSampleTask4();
+
 public:
     TaskStub(void);
     ~TaskStub(void);
+
+    // returns a tasklist containing four sample tasks.
+    // do not use this if you require tasks to adhere to
+    // specific ranges of dates, for reasons such as equivalence 
+    // partioning and boundary value analysis.
+    static TaskList getSampleTaskList();
+
 };
 
