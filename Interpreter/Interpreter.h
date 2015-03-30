4586d08264db0ea0c61eb5d5153ca9cb28c38595
Pham Thi Hong <phamthihong292@hotmail.co.uk>
2015-03-30 10:57:46 +0800

    Update parseSearchCmd

--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -1,6 +1,7 @@
  #pragma once
 #include <string>
 #include "Task.h"
+#include "TaskList.h"
 #include "MappingNumber.h"
 
 #define LENGTH  200
@@ -35,19 +36,18 @@ private:
 	static int month_days(int year, int month); //return number of days in a particular month
 	static void wDaySearch(int year, int month, int day, int *wday); //output: wday:(0-6: Sun,Mon,....,Sat)
 	static void Monthday(int year, int yearDay, int *pMonth, int *pDay); //input: year,yearDay
+	//output: the date of a particular day in a year
 	static Task prepareDelCmd(std::string input);
 	static int ConvertStrtoNum (std::string str);
-	//bool CheckTask (Task _task);
-	//output: the date of a particular day in a year
 
 public:    
     static Task parseAddCmd(std::string input);
     static Task parseEditCmd(std::string input);
     static std::string parseStoreCmd(std::string input);
     static int parseDelCmd(std::string input);
-	//static int ConvertStrtoNum (std::string str);
 	static int parseDoneCmd(std::string input);
 	static std:: string parseViewCmd(std::string input);
+	static TaskList::TList parseSearchCmd(std::string input);
 	static void tmConvert(CalEvent Event, time_t *starttime, time_t *endtime);
 	Interpreter(void);
 	~Interpreter(void);
