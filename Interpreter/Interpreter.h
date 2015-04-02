d4972a645e8b8fcb391def44712c84a60194d3fd
seowyanyi <seow.yanyi@gmail.com>
2015-04-03 00:25:53 +0800

    Fixes erroneous commit c68128bd51dfe06dafca23e158ad79e6e54c24fb

--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -1,6 +1,9 @@
-#pragma once
+ #pragma once
 #include <string>
 #include "Task.h"
+#include "TaskList.h"
+#include "MappingNumber.h"
+
 #define LENGTH  200
 typedef struct Cal
 {
@@ -16,7 +19,7 @@ typedef struct Cal
 class Interpreter
 {
 private:
-	static const size_t NUM_CHARS_STORAGE;
+    static const size_t NUM_CHARS_STORAGE;
 	static const size_t NUM_CHARS_VIEW;
 
 	static bool Search(std::string keyword, Task task){
@@ -28,22 +31,24 @@ private:
 			return false;
 		}
 	}
-	static int parse(std::string event, CalEvent *calEventOut);//event:  input string£»calEventOut: output result
+	static int parse(std::string event, CalEvent *calEventOut);
 	static int IsLeapYear(int year); //return = 1 means leap year
 	static int month_days(int year, int month); //return number of days in a particular month
 	static void wDaySearch(int year, int month, int day, int *wday); //output: wday:(0-6: Sun,Mon,....,Sat)
 	static void Monthday(int year, int yearDay, int *pMonth, int *pDay); //input: year,yearDay
 	//output: the date of a particular day in a year
+	static Task prepareDelCmd(std::string input);
+	static int ConvertStrtoNum (std::string str);
 
-public:
-	static Task parseAddCmd(std::string input);
-	static Task parseEditCmd(std::string input);
-	static std::string parseStoreCmd(std::string input);
-	static Task parseDelCmd(std::string input);
+public:    
+    static Task parseAddCmd(std::string input);
+    static Task parseEditCmd(std::string input);
+    static std::string parseStoreCmd(std::string input);
+    static int parseDelCmd(std::string input);
 	static int parseDoneCmd(std::string input);
-	static std::string parseViewCmd(std::string input);
+	static std:: string parseViewCmd(std::string input);
+	static TaskList::TList parseSearchCmd(std::string input);
 	static void tmConvert(CalEvent Event, time_t *starttime, time_t *endtime);
-
 	Interpreter(void);
 	~Interpreter(void);
 };
