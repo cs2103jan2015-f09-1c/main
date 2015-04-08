83bc77724274c68af78466fb99783d37b3502005
Pham Thi Hong <phamthihong292@hotmail.co.uk>
2015-04-09 01:00:39 +0800

    Create SearchAlias.h

---
+++ Aliases/SearchAlias.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class SearchAlias {
+private:
+	static void removeSpaces(std::string &input);
+
+public:
+	SearchAlias(void);
+	~SearchAlias(void);
+
+	// if there is nothing after the input command, it means
+	// the command is trying show help messages. Return true in this case.
+	static bool isHelp(std::string alias);
+};
