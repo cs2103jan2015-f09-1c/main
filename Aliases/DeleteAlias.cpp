33f5fc786da9318100d250de5a1e42e92fd601d0
Pham Thi Hong <phamthihong292@hotmail.co.uk>
2015-04-08 22:16:43 +0800

    Create DeleteAlias.cpp

---
+++ Aliases/DeleteAlias.cpp
@@ -0,0 +1,33 @@
+#include "DeleteAlias.h"
+#include <algorithm> //std::transform
+#include <sstream>
+
+void DeleteAlias::removeSpaces(std::string &input) {
+  input.erase(std::remove(input.begin(), input.end(), ' '), input.end());
+}
+
+DeleteAlias::DeleteAlias(void) {
+}
+
+
+DeleteAlias::~DeleteAlias(void) {
+}
+
+bool DeleteAlias::isInteger(std::string detail) {
+	int integer;
+	std::stringstream convert (detail);
+	convert >> integer;
+
+	if (convert.fail()){
+		integer = 0;
+	}
+	else{
+		integer = 1 ;
+	}
+	return integer;
+}
+
+bool DeleteAlias::isHelp(std::string detail) {
+	removeSpaces(detail);
+	return detail == "";
+}
