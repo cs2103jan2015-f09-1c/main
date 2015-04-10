a8c3eaea1efdbef35230b39c1f7ac9d4d15569d1
seowyanyi <seow.yanyi@gmail.com>
2015-04-10 08:37:19 +0800

    edit function

--- Command/EditCmd.cpp
+++ Command/EditCmd.cpp
@@ -1,7 +1,8 @@
 #include "EditCmd.h"
 #include "Storage.h"
 #include "TaskList.h"
-
+#include "History.h"
+#include <assert.h>
 
 EditCmd::EditCmd(void) {
 }
@@ -14,44 +15,54 @@ void EditCmd::prepareTask(Task task) {
     _task = task;
 }
 
+void EditCmd::recordInHistory(Task task) {
+    State prevState; 
+    prevState.recordTask (task);
+    History *hist = History::getInstance();
+    hist->saveState(prevState);
+    hist->saveCommand(CommandType::EDIT);
+}
+
+
 UIObject EditCmd::execute() {
     UIObject temp;
 
     Storage* storage = Storage::getInstance();
     TaskList taskList = storage->getTaskList();
-
-	//edit the task
+	recordInHistory(taskList.findTask(_task.getTaskID()));
+	//update storage with the edited task
 	taskList.update(_task);
+    storage->updateStorage(taskList);
 
 	//return UI Object
 	temp.setHeaderText("Successfuly Edited");
-
-	/*		
-    //edit the task
-	TaskList::taskIt it;
-	TaskList::TList _taskList = taskList.getAll();
-    for (it = _taskList.begin(); it != _taskList.end(); ++it) {
-        unsigned targetId = _task.getTaskID();
-        unsigned curId = it->getTaskID();
-
-        if (targetId == curId) { //found
-			temp.setHeaderText("-----Editing task "+std::to_string(it->getTaskID())+"-----");
-			
-        }
-		else{ //not found
-			temp.setHeaderText("-----Task not found-----");			
-		}
-    }
-	*/
-
-    storage->updateStorage(taskList);    
-
-
     return temp;
 }
 
 UIObject EditCmd::undo() {
+	History *hist = History::getInstance();
+
+	CommandType::Command prevCmd = hist->getPreviousCommand();
+    assert(prevCmd == CommandType::EDIT);
+
+	State prevState = hist->getPreviousState();
+	Task prevTask = prevState.getTask();
+
+	Storage* storage = Storage::getInstance();
+    TaskList taskList = storage->getTaskList();
+	taskList.update(prevTask);
+	storage->updateStorage(taskList);    
+
+	hist->clearHistory();
+
     UIObject undoMessage;
-    //undo stuff
+  
+    TaskList::TList restored;
+    restored.push_back(prevTask);
+
+	//return UIObject
+	undoMessage.setHeaderText("Original task restored.");
+	undoMessage.setTaskList(restored);
+
     return undoMessage;
 }
--- Command/EditCmd.h
+++ Command/EditCmd.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "commandinterface.h"
 class EditCmd : public CommandInterface {
+private:
+    void recordInHistory(Task task);
+    Task _task;
+
 public:
     EditCmd(void);
     ~EditCmd(void);
     void prepareTask(Task task);
     UIObject execute();
     UIObject undo();
-
-private:
-    Task _task;
 };
