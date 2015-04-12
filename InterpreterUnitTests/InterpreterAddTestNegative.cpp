6b7a601e5a9759d70728e9c7c6e11568c2d2546c
seowyanyi <seow.yanyi@gmail.com>
2015-04-12 09:53:54 +0800

    more unit tests

--- InterpreterUnitTests/InterpreterAddTestNegative.cpp
+++ InterpreterUnitTests/InterpreterAddTestNegative.cpp
@@ -7,10 +7,85 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace MyCalTestSuite {
 	TEST_CLASS(InterpreterAddNegativeTest) {
 	public:
-		/*TEST_METHOD(TestFloating) {
-            //Interpreter::parseAddCmd("");
+        TEST_METHOD(TestInvalidTime) {
+            bool correctException = false;
 
-        }*/
+            try {
+                Interpreter::parseAddCmd("project meeting :at 1260pm");
+                std::wstring msg( L"No exception has been thrown" );
+                const wchar_t* failMsg = msg.c_str();
+                Assert::Fail(failMsg);
+            } 
+            catch(InvalidInputException &e) {
+                correctException = true;
+            } 
+
+            Assert::IsTrue(correctException);
+        }
+
+        TEST_METHOD(TestDayOutOfRange) {
+            bool correctException = false;
+
+            try {
+                Interpreter::parseAddCmd("project meeting :on 32/01/2015");
+                std::wstring msg( L"No exception has been thrown" );
+                const wchar_t* failMsg = msg.c_str();
+                Assert::Fail(failMsg);
+            } 
+            catch(InvalidInputException &e) {
+                correctException = true;
+            } 
+
+            Assert::IsTrue(correctException);
+        }
+
+        TEST_METHOD(TestMonthOutOfRange) {
+            bool correctException = false;
+
+            try {
+                Interpreter::parseAddCmd("project meeting :on 01/13/2015");
+                std::wstring msg( L"No exception has been thrown" );
+                const wchar_t* failMsg = msg.c_str();
+                Assert::Fail(failMsg);
+            } 
+            catch(InvalidInputException &e) {
+                correctException = true;
+            } 
+
+            Assert::IsTrue(correctException);
+        }
+
+        TEST_METHOD(TestYearUpperLimit) {
+            bool correctException = false;
+
+            try {
+                Interpreter::parseAddCmd("project meeting :on 01/13/2201");
+                std::wstring msg( L"No exception has been thrown" );
+                const wchar_t* failMsg = msg.c_str();
+                Assert::Fail(failMsg);
+            } 
+            catch(InvalidInputException &e) {
+                correctException = true;
+            } 
+
+            Assert::IsTrue(correctException);
+        }
+
+        TEST_METHOD(TestYearLowerLimit) {
+            bool correctException = false;
+
+            try {
+                Interpreter::parseAddCmd("project meeting :on 01/13/1970");
+                std::wstring msg( L"No exception has been thrown" );
+                const wchar_t* failMsg = msg.c_str();
+                Assert::Fail(failMsg);
+            } 
+            catch(InvalidInputException &e) {
+                correctException = true;
+            } 
+
+            Assert::IsTrue(correctException);
+        }
 
 
 		
--- InterpreterUnitTests/InterpreterAddTestPositive.cpp
+++ InterpreterUnitTests/InterpreterAddTestPositive.cpp
@@ -1,26 +1,20 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include <iomanip> // put_time
 #include "CommandType.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-// The interpreter's command options like :at, :from, :to are case insensitive and space-insensitive.
-// Thus you will see test cases with a mix of cases as well as spacings
 
 namespace MyCalTestSuite {
 	TEST_CLASS(InterpreterAddPositiveTest) {
 	public:
 		TEST_METHOD(TestFloating) {
-			std::string floatingInput = "CS2103 tutorial";
-            Task floatingTask = Interpreter::parseAddCmd(floatingInput);
+            Task floatingTask = Interpreter::parseAddCmd("CS2103 tutorial");
             Assert::AreEqual(std::string("CS2103 tutorial"), floatingTask.getTaskName());
             Assert::IsTrue(floatingTask.isFloating()); 
         }
 
-		TEST_METHOD(TestAtOption12Hrs) {
-			std::string atInput = "project meeting :aT       531 pm";
-            Task task = Interpreter::parseAddCmd(atInput);
+		TEST_METHOD(Test12HrsFormat) {
+            Task task = Interpreter::parseAddCmd("project meeting :at 531 pm");
 
             Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -29,40 +23,55 @@ namespace MyCalTestSuite {
             Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
         }
 
-		TEST_METHOD(TestAtOption24Hrs) {
-			std::string atInput = "project meeting  :At          1731  ";
-            Task task = Interpreter::parseAddCmd(atInput);
+		TEST_METHOD(Test24HrsFormat) {
+            Task task = Interpreter::parseAddCmd("project meeting :at 1731");
 
-            Assert::AreEqual(std::string("project meeting  "), task.getTaskName());
+            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
             Assert::AreEqual(std::string("05:31 PM"), task.getBeginStr());
             Assert::AreEqual(std::string("07:31 PM"), task.getEndStr());
             Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
         }
 
+		TEST_METHOD(TestDateSeparatorFormats) {
+            Task task = Interpreter::parseAddCmd("project :on 11.12.2014");
 
-		TEST_METHOD(TestSpaceAfterColon) {
-			std::string atInput = "project meeting :   at       531 pm";
-            Task task = Interpreter::parseAddCmd(atInput);
+            Assert::AreEqual(std::string("project "), task.getTaskName());
+            Assert::IsFalse(task.isFloating()); 
+            Assert::AreEqual(std::string("08:00 AM"), task.getBeginStr());
+            Assert::AreEqual(std::string("10:00 AM"), task.getEndStr());
+            Assert::AreEqual(std::string("Thu Dec 11 2014"), task.getDateStr());
 
-            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
-            Assert::IsTrue(task.isFloating()); 
+            Task task2 = Interpreter::parseAddCmd("project :on 11/12/2014");
+            Assert::AreEqual(std::string("Thu Dec 11 2014"), task2.getDateStr());
+
+            Task task3 = Interpreter::parseAddCmd("project :on 11\\12.2014");
+            Assert::AreEqual(std::string("Thu Dec 11 2014"), task3.getDateStr());
         }
 
-		TEST_METHOD(TestFromOption) {
-			std::string fromInput = "project meeting :from      309    am";
-            Task task = Interpreter::parseAddCmd(fromInput);
+		TEST_METHOD(TestFromOptionOnly) {
+            Task task = Interpreter::parseAddCmd("project meeting :from 309am");
 
             Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
             Assert::AreEqual(std::string("03:09 AM"), task.getBeginStr());
-            //Assert::AreEqual(std::string("10:40 PM"), atTask.getEndStr());
+            Assert::AreEqual(std::string("05:09 AM"), task.getEndStr());
+            Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
+        }
+
+
+		TEST_METHOD(TestToOptionOnly) {
+            Task task = Interpreter::parseAddCmd("project meeting :to 309am");
+
+            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
+            Assert::IsFalse(task.isFloating()); 
+            Assert::AreEqual(std::string("01:09 AM"), task.getBeginStr());
+            Assert::AreEqual(std::string("03:09 AM"), task.getEndStr());
             Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
         }
 
 		TEST_METHOD(TestFromToOption) {
-			std::string fromInput = "project meeting :from 1021am :to 1pm";
-            Task task = Interpreter::parseAddCmd(fromInput);
+            Task task = Interpreter::parseAddCmd("project meeting :from 1021am :to 1pm");
 
             Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -71,9 +80,39 @@ namespace MyCalTestSuite {
             Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
         }
 
+
+		TEST_METHOD(TestFromToAlternateFormat) {
+            Task task = Interpreter::parseAddCmd("project meeting :from 1021am-1pm");
+
+            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
+            Assert::IsFalse(task.isFloating()); 
+            Assert::AreEqual(std::string("10:21 AM"), task.getBeginStr());
+            Assert::AreEqual(std::string("01:00 PM"), task.getEndStr());
+            Assert::AreEqual(TaskStub::getCurrentDate(), task.getDateStr());
+        }
+
+		TEST_METHOD(TestCaseInsensitivity) {
+            Task task = Interpreter::parseAddCmd("project meeting :oN 13/08/2015 :FRoM 1021am :To 1pm");
+
+            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
+            Assert::IsFalse(task.isFloating()); 
+            Assert::AreEqual(std::string("10:21 AM"), task.getBeginStr());
+            Assert::AreEqual(std::string("01:00 PM"), task.getEndStr());
+            Assert::AreEqual(std::string("Thu Aug 13 2015"), task.getDateStr());
+        }
+
+		TEST_METHOD(TestSpaceInsensitivity) {
+            Task task = Interpreter::parseAddCmd("project : o n  1 3 / 0  8 /  2  0 15    : f r om  1 02 1 a m   :to   1p  m");
+
+            Assert::AreEqual(std::string("project "), task.getTaskName());
+            Assert::IsFalse(task.isFloating()); 
+            Assert::AreEqual(std::string("10:21 AM"), task.getBeginStr());
+            Assert::AreEqual(std::string("01:00 PM"), task.getEndStr());
+            Assert::AreEqual(std::string("Thu Aug 13 2015"), task.getDateStr());
+        }
+
 		TEST_METHOD(TestWithoutTime) {
-			std::string input = "project meeting :oN 30/07/2014";
-            Task task = Interpreter::parseAddCmd(input);
+            Task task = Interpreter::parseAddCmd("project meeting :on 30/07/2014");
 
             Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -83,8 +122,7 @@ namespace MyCalTestSuite {
         }
 
 		TEST_METHOD(TestTomorrowOption) {
-			std::string input = "project meeting :tomorrow";
-            Task task = Interpreter::parseAddCmd(input);
+            Task task = Interpreter::parseAddCmd("project meeting :tomorrow");
 
             Assert::AreEqual(std::string("project meeting "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -94,8 +132,7 @@ namespace MyCalTestSuite {
         }
 
 		TEST_METHOD(TestTomorrowFromToOption) {
-			std::string input = "blah :toMoRrow :FroM 7am :TO 932pm";
-            Task task = Interpreter::parseAddCmd(input);
+            Task task = Interpreter::parseAddCmd("blah :tomorrow :from 7am :to 932pm");
 
             Assert::AreEqual(std::string("blah "), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -106,16 +143,14 @@ namespace MyCalTestSuite {
 
         // Test boundary of dates, e.g. last day, first day of months 
         TEST_METHOD(TestJanBoundary) {
-			std::string firstDay = "blah :on 01/01/2015";
-            Task taskFirstDay = Interpreter::parseAddCmd(firstDay);
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/01/2015");
             Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
             Assert::IsFalse(taskFirstDay.isFloating()); 
             Assert::AreEqual(std::string("Thu Jan 01 2015"), taskFirstDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
 
-			std::string lastDay = "blah :on 31/01/2015";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 31/01/2015");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
             Assert::AreEqual(std::string("Sat Jan 31 2015"), tasklastDay.getDateStr());
@@ -124,16 +159,14 @@ namespace MyCalTestSuite {
         }
 
         TEST_METHOD(TestFebBoundary) {
-			std::string firstDay = "blah :on 01/02/2015";
-            Task taskFirstDay = Interpreter::parseAddCmd(firstDay);
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/02/2015");
             Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
             Assert::IsFalse(taskFirstDay.isFloating()); 
             Assert::AreEqual(std::string("Sun Feb 01 2015"), taskFirstDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
 
-			std::string lastDay = "blah :on 28/02/2015";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 28/02/2015");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
             Assert::AreEqual(std::string("Sat Feb 28 2015"), tasklastDay.getDateStr());
@@ -142,16 +175,14 @@ namespace MyCalTestSuite {
         }
 
         TEST_METHOD(TestJuneBoundary) {
-			std::string firstDay = "blah :on 01/06/2015";
-            Task taskFirstDay = Interpreter::parseAddCmd(firstDay);
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/06/2015");
             Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
             Assert::IsFalse(taskFirstDay.isFloating()); 
             Assert::AreEqual(std::string("Mon Jun 01 2015"), taskFirstDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
 
-			std::string lastDay = "blah :on 30/06/2015";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 30/06/2015");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
             Assert::AreEqual(std::string("Tue Jun 30 2015"), tasklastDay.getDateStr());
@@ -160,16 +191,14 @@ namespace MyCalTestSuite {
         }
 
         TEST_METHOD(TestDecBoundary) {
-			std::string firstDay = "blah :on 01/12/2015";
-            Task taskFirstDay = Interpreter::parseAddCmd(firstDay);
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/12/2015");
             Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
             Assert::IsFalse(taskFirstDay.isFloating()); 
             Assert::AreEqual(std::string("Tue Dec 01 2015"), taskFirstDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
 
-			std::string lastDay = "blah :on 31/12/2015";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 31/12/2015");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
             Assert::AreEqual(std::string("Thu Dec 31 2015"), tasklastDay.getDateStr());
@@ -178,8 +207,7 @@ namespace MyCalTestSuite {
         }
 
         TEST_METHOD(TestLeapDay) {
-			std::string lastDay = "blah :on    29/02/2016";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 29/02/2016");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
             Assert::AreEqual(std::string("Mon Feb 29 2016"), tasklastDay.getDateStr());
@@ -188,26 +216,39 @@ namespace MyCalTestSuite {
         }
 
         TEST_METHOD(TestPastDate) {
-			std::string firstDay = "blah :on 01/01/2010";
-            Task taskFirstDay = Interpreter::parseAddCmd(firstDay);
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/01/1971");
             Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
             Assert::IsFalse(taskFirstDay.isFloating()); 
-            Assert::AreEqual(std::string("Fri Jan 01 2015"), taskFirstDay.getDateStr());
+            Assert::AreEqual(std::string("Fri Jan 01 1971"), taskFirstDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
 
-			std::string lastDay = "blah :on 31/12/2010";
-            Task tasklastDay = Interpreter::parseAddCmd(lastDay);
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 31/12/1971");
             Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
             Assert::IsFalse(tasklastDay.isFloating()); 
-            Assert::AreEqual(std::string("Fri Dec 31 2010"), tasklastDay.getDateStr());
+            Assert::AreEqual(std::string("Fri Dec 31 1971"), tasklastDay.getDateStr());
+            Assert::AreEqual(std::string("08:00 AM"), tasklastDay.getBeginStr());
+            Assert::AreEqual(std::string("10:00 AM"), tasklastDay.getEndStr());
+        }
+
+        TEST_METHOD(TestFutureDate) {
+            Task taskFirstDay = Interpreter::parseAddCmd("blah :on 01/01/2200");
+            Assert::AreEqual(std::string("blah "), taskFirstDay.getTaskName());
+            Assert::IsFalse(taskFirstDay.isFloating()); 
+            Assert::AreEqual(std::string("Wed Jan 01 2200"), taskFirstDay.getDateStr());
+            Assert::AreEqual(std::string("08:00 AM"), taskFirstDay.getBeginStr());
+            Assert::AreEqual(std::string("10:00 AM"), taskFirstDay.getEndStr());
+
+            Task tasklastDay = Interpreter::parseAddCmd("blah :on 31/12/2200");
+            Assert::AreEqual(std::string("blah "), tasklastDay.getTaskName());
+            Assert::IsFalse(tasklastDay.isFloating()); 
+            Assert::AreEqual(std::string("Wed Dec 31 2200"), tasklastDay.getDateStr());
             Assert::AreEqual(std::string("08:00 AM"), tasklastDay.getBeginStr());
             Assert::AreEqual(std::string("10:00 AM"), tasklastDay.getEndStr());
         }
 
         TEST_METHOD(TestColonInTaskName) {
-			std::string input = "project me:eting :oN 30/07/2014 :from 1145pm to 2359";
-            Task task = Interpreter::parseAddCmd(input);
+            Task task = Interpreter::parseAddCmd("project me:eting :on 30/07/2014 :from 1145pm to 2359");
 
             Assert::AreEqual(std::string("project me"), task.getTaskName());
             Assert::IsFalse(task.isFloating()); 
@@ -216,43 +257,12 @@ namespace MyCalTestSuite {
             Assert::AreEqual(std::string("11:59 PM"), task.getEndStr());
         }
         
-        TEST_METHOD(TestInvalidTime) {
-			std::string input = "project meeeting :oN 30/07/2014 :from 1145pm to 2359";
-            Task task = Interpreter::parseAddCmd(input);
+        // If an unrecognized option is entered, task will default to floating
+        TEST_METHOD(TestUnrecognizedOption) {        
+            Task task = Interpreter::parseAddCmd("project meeting :zzz 05/01/2015");
 
-            Assert::AreEqual(std::string("project me"), task.getTaskName());
-            Assert::IsFalse(task.isFloating()); 
-            Assert::AreEqual(std::string("Wed Jul 30 2014"), task.getDateStr());
-            Assert::AreEqual(std::string("11:45 PM"), task.getBeginStr());
-            Assert::AreEqual(std::string("11:59 PM"), task.getEndStr());
-
+            Assert::AreEqual(std::string("project meeting "), task.getTaskName());
+            Assert::IsTrue(task.isFloating());     
         }
-
-
-         /*  
-
-		TEST_METHOD (TestParseSearchCmd){
-			std::string input = "search meeting";
-			TaskList::TList list;
-			std::string keyword = CommandType::filterOutCmd(input); 
-			list = Interpreter::parseSearchCmd(keyword);
-
-			for (int i=0; i<list.size(); i++){
-			Task task = list.front();
-			std::string taskName = task.getTaskName();
-			int TaskId = task.getTaskID();
-			if (TaskId > 0){
-			Assert::IsTrue(check_substring(keyword, taskName));
-			//Check if the task name contain the key word "meeting"
-			}
-			else if (TaskId <= 0 ){
-				//Check if the task name contain the key word "meeting"
-				Assert::IsFalse(check_substring(keyword, taskName));
-			}
-			list.pop_front();
-			}
-		}
-        
-        */
 	};
 }
